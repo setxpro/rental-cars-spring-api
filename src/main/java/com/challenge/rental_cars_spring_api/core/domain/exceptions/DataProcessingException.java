e507ef0b9cd6575dd78d60f5ebdf623fb4325d77
Patrick Anjos <patrickpqdt87289@gmail.com>
2024-12-19 13:12:35 -0300

    remove exceptions

--- src/main/java/com/challenge/rental_cars_spring_api/core/domain/exceptions/DataProcessingException.java
+++
@@ -1,7 +0,0 @@
-package com.challenge.rental_cars_spring_api.core.domain.exceptions;
-
-public class DataProcessingException extends RuntimeException {
-    public DataProcessingException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/challenge/rental_cars_spring_api/core/domain/exceptions/InvalidDataException.java
+++
@@ -1,7 +0,0 @@
-package com.challenge.rental_cars_spring_api.core.domain.exceptions;
-
-public class InvalidDataException extends RuntimeException {
-    public InvalidDataException(String message) {
-        super(message);
-    }
-}
